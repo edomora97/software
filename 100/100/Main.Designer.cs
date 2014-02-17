e4aaf30375b367db5110385045c2d3784b29870e
Edoardo <gianluigi988@gmail.com>
2014-02-17 16:56:31 +0100

    Aggiunto 100

---
+++ 100/100/Main.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace _100
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(563, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tempo:";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(535, 22);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(98, 37);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "00:00";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUndo.Location = new System.Drawing.Point(535, 62);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(99, 52);
+            this.btnUndo.TabIndex = 2;
+            this.btnUndo.Text = "UNDO";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(535, 120);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(99, 52);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(559, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(550, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Selezionabile";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(553, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Selezionato";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(559, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "0";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Non selezionabile";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(559, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(524, 498);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "® Edoardo Morassutto";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 520);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "100";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 100/100/Main.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _100
+{
+    public partial class Main : Form
+    {
+        Casella[,] caselle = new Casella[10, 10];
+        State[] undoList = new State[100];
+        int count = 0;
+        bool isStarted = false;
+        DateTime startTime;
+
+        public Main()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            InitializeComponent();
+            caselle = new Casella[10, 10];
+            undoList = new State[100];
+            count = 0;
+            isStarted = false;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button currButton = new Button();
+                    currButton.Name = "btn" + i.ToString() + j.ToString();
+                    currButton.Bounds = new Rectangle(i * 50 + 10, j * 50 + 10, 50, 50);
+                    currButton.BackColor = Color.White;
+                    currButton.Click += new EventHandler(Button_Click);
+
+                    Casella casella = new Casella();
+                    casella.button = currButton;
+                    casella.colore = Colore.Verde;
+                    casella.value = -1;
+                    casella.x = i;
+                    casella.y = j;
+
+                    caselle[i, j] = casella;
+                    this.Controls.Add(currButton);
+                }
+            }
+
+            State stato = new State();
+            stato.caselle = caselle;
+            stato.x = -1;
+            stato.y = -1;
+            undoList[count] = stato;
+
+            btnUndo.Enabled = false;
+            RefreshButtons();
+        }
+
+        void Reset()
+        {
+            while (count > 0)
+            {
+                undoList[count] = new State();
+                count--;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    caselle[i, j].colore = Colore.Verde;
+                    caselle[i, j].value = -1;
+                }
+            }
+            RefreshButtons();
+            State stato = new State();
+            stato.caselle = caselle;
+            stato.x = -1;
+            stato.y = -1;
+            undoList[count] = stato;
+            lblTimer.Text = "00:00";
+            isStarted = false;
+            btnUndo.Enabled = false;
+        }
+
+        void Button_Click(object sender, EventArgs e)
+        {            
+            int x = 0, y = 0;
+
+            Button btn = (Button)sender;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i * 50 + 10 == btn.Bounds.X)
+                        if (j * 50 + 10 == btn.Bounds.Y)
+                        {
+                            x = i;
+                            y = j;
+                            break;
+                        }
+                }
+            } 
+            if (isStarted == false)
+            {
+                isStarted = true;
+                timer.Start();
+                startTime = DateTime.Now;                
+            }
+            if (caselle[x, y].colore == Colore.Verde)
+            {
+                State stato = new State();
+                stato.caselle = new Casella[10, 10];
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        stato.caselle[i, j] = caselle[i, j];
+                    }
+                }
+                stato.x = x;
+                stato.y = y;
+                undoList[count] = stato;
+
+                btnUndo.Enabled = true;
+                int possibilità = 0;
+                caselle[x, y].button.BackColor = Color.Red;
+                caselle[x, y].colore = Colore.Rosso;
+                count++;
+                caselle[x, y].value = count;
+                caselle[x, y].button.Text = count.ToString();
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (caselle[i, j].value == -1)
+                        {
+                            caselle[i, j].button.BackColor = Color.White;
+                            caselle[i, j].colore = Colore.Bianco;
+                        }
+                    }
+                }
+
+                try
+                {
+                    if (caselle[x, y - 3].colore == Colore.Bianco)
+                    {
+                        caselle[x, y - 3].colore = Colore.Verde;
+                        caselle[x, y - 3].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x, y + 3].colore == Colore.Bianco)
+                    {
+                        caselle[x, y + 3].colore = Colore.Verde;
+                        caselle[x, y + 3].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x - 3, y].colore == Colore.Bianco)
+                    {
+                        caselle[x - 3, y].colore = Colore.Verde;
+                        caselle[x - 3, y].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x + 3, y].colore == Colore.Bianco)
+                    {
+                        caselle[x + 3, y].colore = Colore.Verde;
+                        caselle[x + 3, y].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x - 2, y - 2].colore == Colore.Bianco)
+                    {
+                        caselle[x - 2, y - 2].colore = Colore.Verde;
+                        caselle[x - 2, y - 2].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x + 2, y + 2].colore == Colore.Bianco)
+                    {
+                        caselle[x + 2, y + 2].colore = Colore.Verde;
+                        caselle[x + 2, y + 2].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x - 2, y + 2].colore == Colore.Bianco)
+                    {
+                        caselle[x - 2, y + 2].colore = Colore.Verde;
+                        caselle[x - 2, y + 2].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (caselle[x + 2, y - 2].colore == Colore.Bianco)
+                    {
+                        caselle[x + 2, y - 2].colore = Colore.Verde;
+                        caselle[x + 2, y - 2].button.BackColor = Color.Green;
+                        possibilità++;
+                    }
+                }
+                catch { }
+
+
+                if (possibilità == 0 && count < 100)
+                {
+                    timer.Stop(); 
+                    DateTime currentTime = DateTime.Now;
+                    TimeSpan time = currentTime - startTime;
+                    MessageBox.Show("Mi dispiace, hai perso!\nSei arrivato alla casella " + count.ToString() + "\nCi hai messo " + time.Minutes.ToString() + " minuti e " + time.Seconds.ToString() + " secondi");
+                }
+                if (count >= 100)
+                {
+                    timer.Stop();
+                    DateTime currentTime = DateTime.Now;
+                    TimeSpan time = currentTime - startTime;
+                    MessageBox.Show("CONGRATULAZIONI, hai vinto!!\nCi hai messo " + time.Minutes.ToString() + " minuti e " + time.Seconds.ToString() + " secondi");
+                }
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            TimeSpan time = currentTime - startTime;
+            lblTimer.Text = (time.Minutes < 10 ? "0" + time.Minutes.ToString() : time.Minutes.ToString()) + ":" + (time.Seconds < 10 ? "0" + time.Seconds.ToString() : time.Seconds.ToString());
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            //undoList.RemoveAt(undoList.Count - 1);
+            undoList[count] = new State();
+            count--;
+            //State stato = undoList[undoList.Count - 1];
+            State stato = undoList[count];
+            if (stato.caselle.Equals(caselle))
+                System.Diagnostics.Debug.WriteLine("Sono uguali");
+            caselle = stato.caselle;
+            RefreshButtons();
+            //if (undoList.Count == 1)
+            if (count == 0)
+                btnUndo.Enabled = false;
+        }
+
+        void RefreshButtons()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    string nome = "btn" + x.ToString() + y.ToString();
+                    Controls[nome].BackColor = caselle[x, y].colore == Colore.Bianco ? Color.White : caselle[x, y].colore == Colore.Verde ? Color.Green : Color.Red;
+                    Controls[nome].Text = caselle[x, y].value.ToString() == "-1" ? "" : caselle[x, y].value.ToString();
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            Reset();
+        }
+        
+    }
+    public enum Colore
+    {
+        Rosso, Verde, Bianco
+    }
+    public struct Casella
+    {
+        public Colore colore;
+        public Button button;
+        public int x;
+        public int y;
+        public int value;
+    }
+    public struct State
+    {
+        public Casella[,] caselle;
+        public int x, y;
+    }
+}
